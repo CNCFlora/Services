c477d4b2abe6119bfa84976152f965dfa13d4fa1
Diogo Souza <diogo@diogok.net>
2013-12-06 14:11:35 -0200

    some more docker touches

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 #service nginx start
 APP_USER=cncflora
-su $APP_USER -c 'cd ~/www && CONFIG=/root/config.yml RACK_ENV=production nohup rackup &'
+su $APP_USER -c 'cd ~/www && RACK_ENV=production nohup rackup &'
 
 /usr/sbin/sshd -D
 
