721909d7e85d090fad094b6105590e0824043b48
Diogo Souza <diogo@diogok.net>
2013-12-11 15:08:00 -0200

    adjusting docker

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-docker run -p 8181:9292 -p 2424:22 -t cncflora/services /root/start.sh
+OPTS=""
+[[ $BASE_URL ]] && OPTS="$OPTS -e BASE_URL=$BASE_URL"
+[[ $COUCHDB ]] && OPTS="$OPTS -e COUCHDB=$COUCHDB"
+[[ $ESEARCH ]] && OPTS="$OPTS -e ESEARCH=$ESEARCH"
+
+docker run -d -p 8181:9292 -p 2424:22 $OPTS -t cncflora/services /root/start.sh
 
--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+OPTS="RACK_ENV=production"
+[[ $BASE_URL ]] && OPTS="$OPTS BASE_URL=$BASE_URL"
+[[ $COUCHDB ]] && OPTS="$OPTS COUCHDB=$COUCHDB"
+[[ $ESEARCH ]] && OPTS="$OPTS ESEARCH=$ESEARCH"
+
 #service nginx start
 APP_USER=cncflora
-su $APP_USER -c 'cd ~/www && RACK_ENV=production nohup rackup &'
+su $APP_USER -c "cd ~/www && $OPTS nohup rackup &"
 
 /usr/sbin/sshd -D
 
